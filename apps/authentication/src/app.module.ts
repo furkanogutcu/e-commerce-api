ad78d02e5c8bda559b871a3a10fed3320097f57e
furkanogutcu <ogutcu@bordo.io>
2023-08-05 22:05:57 +0300

    feat(auth): nestjs microservice app setup

--- apps/authentication/src/app.module.ts
+++ apps/authentication/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+      isGlobal: true,
+      envFilePath: `.env${process.env.NODE_ENV === 'test' ? '.test' : ''}`,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- apps/authentication/src/main.ts
+++ apps/authentication/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: `${process.env.HOST}:${process.env.PORT}`,
+      package: 'authentication',
+      protoPath: join(__dirname, './proto/authentication.proto'),
+    },
+  });
+
+  await app.listen();
 }
 bootstrap();

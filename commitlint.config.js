41354af5dd624c96707cc12a31c3c597cfa9bfe5
furkanogutcu <ogutcu@bordo.io>
2023-08-05 15:52:36 +0300

    chore(global): add editorconfig

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-case': [2, 'always', 'lower-case'],
-        'scope-enum': [2, 'always', ['global']],
-        'scope-empty': [2, 'never']
-    }
-}
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-case": [2, "always", "lower-case"],
+    "scope-enum": [2, "always", ["global"]],
+    "scope-empty": [2, "never"],
+  },
+};
